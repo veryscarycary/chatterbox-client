f36fb4c7b77f1fb97ee2a8989355afb6ce5ab840
veryscarycary <carymeskell@gmail.com>
2016-08-08 20:47:05 -0700

    Implement post and get functionality

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -15,6 +15,9 @@
 //     console.error('chatterbox: Failed to send message', data);
 //   }
 // });
+
+window.user = window.location.search.slice(10).replace(/%20/g, ' ');
+
 var Message = function (message) {
   var result = '';
   result += '<div class="username">' + message.username + '</div>';
@@ -34,6 +37,7 @@ app.init = function () {
 };
 
 app.send = function (message) {
+  console.log(message);
   $.ajax({
     // This is the url you should use to communicate with the parse API server.
     url: 'https://api.parse.com/1/classes/messages',
@@ -54,9 +58,39 @@ app.fetch = function () {
   var result = $.ajax({
     // This is the url you should use to communicate with the parse API server.
     url: 'https://api.parse.com/1/classes/messages',
-    type: 'GET'
+    type: 'GET',
+    data: 'json',
+    success: function(data) {
+      console.log('success', data);
+    }
   });
+  //needs to add message
+  console.log(result);
+  return result.responseText;
 };
+
+// window.getMessages = function(object) {
+//   var messages = object.responseJSON.results;
+
+//   for (var i = 0; i < messages.length; i++) {
+//     $('body').append(messages[i]);
+//   }
+// };
+
+setInterval(function() {
+  $.ajax({
+    // This is the url you should use to communicate with the parse API server.
+    url: 'https://api.parse.com/1/classes/messages',
+    type: 'GET',
+    data: 'json',
+    success: function ( data ) {
+      app.clearMessages();
+      for ( var i = 10; i >= 0; i-- ) {
+        app.addMessage( data.results[i] );
+      }
+    }
+  });
+}, 1000);
 
 app.clearMessages = function () {
   $('#chats').children().remove();
@@ -66,7 +100,7 @@ app.addMessage = function ( object ) {
   // debugger;
   // var message = '<div class='message'> message.text';
   var message = Message( object );
-  $('#chats').append(message);
+  $('#chats').prepend(message);
 };
 
 app.addRoom = function (room) {
@@ -78,17 +112,23 @@ app.addFriend = function ( string ) {
 
 };
 
-app.handleSubmit = function () {
-
+app.handleSubmit = function (text) {
+  app.send({username: window.user, text: text, roomname: 'lobby'});
+  //need to update roomname
 };
 
 $(document).ready( function () {
   $('body').on('click', '.username', function () {
     app.addFriend( $(this).text());
+    console.log($('#message-box').val());
   });
 
-  $('.submit').on('submit', function () {
-    app.handleSubmit();
+  $('body').on('click', '.submit', function () {
+    var text = $('#message-box').val();
+    $('#message-box').val('');
+    console.log('hit');
+    app.handleSubmit(text);
+    //need to update roomname
   });
 });
 
